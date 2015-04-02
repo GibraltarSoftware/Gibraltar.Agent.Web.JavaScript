80375c709c148cd3cf9e7cf259b539a722269e06
NathanGloyn <nathans.dropbox@gmail.com>
2015-04-02 17:31:25 +0100

    Alter agent to handle case where it is unable to get hold of platform and adding test for simple uncaught error

---
+++ Tests/specs/When_logging_simple_uncaught_error.js
@@ -0,0 +1,47 @@
+﻿describe('When_logging_simple_uncaught_error', function () {
+
+    var xhr, requests;
+
+
+    beforeEach(function (done) {
+        xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
+        xhr.onCreate = function(req) {
+            requests.push(req);
+        };
+        createSimpleError();
+        requestComplete(done);
+    });
+
+    afterEach(function() {
+        xhr.restore();
+    });
+
+
+    it('Should POST request to correct url', function () {
+
+        var body = JSON.parse(requests[0].requestBody);
+
+        expect(requests[0].url).toBe(window.location.origin + '/Gibraltar/Log/Exception');
+        expect(requests[0].method).toBe('POST');
+        expect(body.Message).toBe("uncaught exception: Test Error");
+    });
+
+
+    function createSimpleError() {
+        setTimeout(function() {
+            throw "Test Error";
+        }, 5);
+
+    }
+
+    function requestComplete(done) {
+        if (requests.length > 0) {
+            done();
+        } else {
+            setTimeout(requestComplete, 10, done);
+        }
+        
+    }
+
+});
